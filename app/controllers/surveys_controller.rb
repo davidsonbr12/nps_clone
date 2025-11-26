0580979e9d968e524eabcb52925106a6b8efd1d0
briandavidson <bda1107@wgu.edu>
2025-11-25 19:29:27 -0800

    fixing syntax disagreements with rubocop

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -2,5 +2,4 @@ class SurveysController < ApplicationController
   # This ensures a user must be signed in AND have the 'admin' role to access any action here
   before_action :authenticate_user!
   before_action :authenticate_admin!
-
 end
